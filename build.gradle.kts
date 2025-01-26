ba3af3c76b61169b9e7481d600d9729b0f673951
Revxrsal <aloosh.kasasbeh@gmail.com>
2025-01-27 00:09:12 +0300

    add maven publisher

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,8 @@
+import com.vanniktech.maven.publish.SonatypeHost
+
 plugins {
     id("java")
+    id("com.vanniktech.maven.publish") version "0.29.0"
 }
 
 group = "io.github.revxrsal"
@@ -19,3 +22,38 @@ dependencies {
     annotationProcessor("org.projectlombok:lombok:1.18.36")
 }
 
+mavenPublishing {
+    coordinates(
+        groupId = group as String,
+        artifactId = "bubbles",
+        version = version as String
+    )
+    pom {
+        name.set("Bubbles")
+        description.set("A library for generating beautiful, commented, type-safe YML through interfaces")
+        inceptionYear.set("2024")
+        url.set("https://github.com/Revxrsal/Bubbles/")
+        licenses {
+            license {
+                name.set("MIT")
+                url.set("https://mit-license.org/")
+                distribution.set("https://mit-license.org/")
+            }
+        }
+        developers {
+            developer {
+                id.set("revxrsal")
+                name.set("Revxrsal")
+                url.set("https://github.com/Revxrsal/")
+            }
+        }
+        scm {
+            url.set("https://github.com/Revxrsal/Bubbles/")
+            connection.set("scm:git:git://github.com/Revxrsal/Bubbles.git")
+            developerConnection.set("scm:git:ssh://git@github.com/Revxrsal/Bubbles.git")
+        }
+    }
+
+    publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL)
+    signAllPublications()
+}
