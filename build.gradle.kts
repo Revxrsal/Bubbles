526ef77d98d5fdb44497c8538f0204b8ab3314e2
Revxrsal <aloosh.kasasbeh@gmail.com>
2025-01-26 14:57:00 +0300

    gradle info

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,19 +3,17 @@ plugins {
 }
 
 group = "io.github.revxrsal"
-version = "1.0-SNAPSHOT"
+version = "1.0"
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
-    implementation("com.squareup.moshi:moshi:1.15.2")
-    implementation("org.ow2.asm:asm:9.7.1")
-    implementation("org.ow2.asm:asm-util:9.7.1")
-    implementation("org.ow2.asm:asm-commons:9.7.1")
-    implementation("org.yaml:snakeyaml:2.0")
-    implementation("com.google.code.gson:gson:2.11.0")
+    compileOnly("org.ow2.asm:asm:9.7.1")
+    compileOnly("org.ow2.asm:asm-commons:9.7.1")
+    compileOnly("org.yaml:snakeyaml:2.0")
+    compileOnly("com.google.code.gson:gson:2.11.0")
     compileOnly("org.jetbrains:annotations:26.0.1")
     compileOnly("org.projectlombok:lombok:1.18.36")
     annotationProcessor("org.projectlombok:lombok:1.18.36")
